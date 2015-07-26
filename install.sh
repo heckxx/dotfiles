a2fab3a954064a34dac47ad87c53dec0c9ca314f
Edward Zhou <edwardzhou@gmail.com>
2015-07-26 00:54:10 -0400

    initial commit

---
+++ install.sh
@@ -0,0 +1,42 @@
+
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+conf="scripts awesome compton.conf"
+files="bashrc ncmpcpp tmux.conf vim/colors vim/bundle/Vundle.vim vimperatorrc vimrc xbindkeysrc xinitc Xmodmap Xresources zshrc"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Installing /home dotfiles"
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+echo "Installing .config dotfiles"
+for conf in $conf; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$conf ~/dotfiles_old/
+    echo "Creating symlink to $conf in home directory."
+    ln -s $dir/$conf ~/.$conf
+done
+echo "Done!"
